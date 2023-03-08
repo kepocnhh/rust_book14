3e718bc8cee8244ad186c8d02a71967f08d583e0
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-08 19:50:03 +0300

    Panics/Errors

---
+++ src/lib.rs
@@ -0,0 +1,93 @@
+/// Returns the contained first value or computes it from a closure.
+///
+/// # Examples
+///
+/// ```
+/// let list = vec![1, 2, 3];
+/// let answer = rust_book14::get_first_or_else(&list, || &-1);
+/// assert_eq!(1, *answer);
+///
+/// let list = vec![];
+/// let answer = rust_book14::get_first_or_else(&list, || &-1);
+/// assert_eq!(-1, *answer);
+/// ```
+pub fn get_first_or_else<'a, T, F: FnOnce() -> &'a T>(list: &'a Vec<T>, f: F) -> &'a T {
+    if list.is_empty() {
+        return f();
+    }
+    return &list[0];
+    // return list.get(0).unwrap_or_else(f);
+}
+
+/// Returns the contained first value or a provided default.
+///
+/// # Examples
+///
+/// ```
+/// let list = vec![1, 2, 3];
+/// let answer = rust_book14::get_first_or(&list, &-1);
+/// assert_eq!(1, *answer);
+///
+/// let list = vec![];
+/// let answer = rust_book14::get_first_or(&list, &-1);
+/// assert_eq!(-1, *answer);
+/// ```
+pub fn get_first_or<'a, T>(list: &'a Vec<T>, default: &'a T) -> &'a T {
+    if list.is_empty() {
+        return default;
+    }
+    return &list[0];
+    // return list.get(0).unwrap_or(default);
+}
+
+/// Returns the contained first [`Some(value)`] or a provided [`Err(err)`].
+///
+/// # Errors
+///
+/// If the `list` is empty, an [`Err(err)`] will be returned.
+///
+/// # Examples
+///
+/// ```
+/// let list = vec![1, 2, 3];
+/// let answer = rust_book14::get_first_or_error(&list, "foo");
+/// assert_eq!(Ok(&1), answer);
+///
+/// let list: Vec<u8> = vec![];
+/// let answer = rust_book14::get_first_or_error(&list, "foo");
+/// assert_eq!(Err("foo"), answer);
+/// ```
+pub fn get_first_or_error<T, U>(list: &Vec<T>, err: U) -> Result<&T, U> {
+    if list.is_empty() {
+        return Err(err);
+    }
+    return Ok(&list[0]);
+    // return list.get(0).ok_or(err);
+}
+
+/// Returns the contained first value or panics with a `msg`.
+///
+/// # Panics
+///
+/// If the `list` is empty, panics with a `msg`.
+///
+/// # Examples
+///
+/// ```
+/// let list = vec![1, 2, 3];
+/// let answer = rust_book14::get_first_or_panic(&list, "foo");
+/// assert_eq!(&1, answer);
+///
+/// let list: Vec<u8> = vec![];
+/// let result = std::panic::catch_unwind(|| {
+///     rust_book14::get_first_or_panic(&list, "foo")
+/// });
+/// assert!(result.is_err());
+/// ```
+pub fn get_first_or_panic<'a, T>(list: &'a Vec<T>, msg: &str) -> &'a T {
+    if list.is_empty() {
+        panic!("{msg}");
+    }
+    return &list[0];
+    // return list.get(0).expect(msg);
+}
