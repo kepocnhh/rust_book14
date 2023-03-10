7f8b27e0ba5d9acfcdf244dd6ed069fb36684b3c
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-10 19:18:59 +0300

    fix documentation 0.1.0

---
+++ doc/0.1.0/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"rust_book14":{"doc":"<code>Rust</code> book lessons. Chapter 14.","t":[5,5,5,5,0,5],"n":["get_first_or","get_first_or_else","get_first_or_error","get_first_or_panic","slices","get_first_or_else"],"q":["rust_book14","","","","","rust_book14::slices"],"d":["Returns the contained first value or a provided default. …","Returns the contained first value or computes it from a …","Returns the contained first <code>Some(value)</code> or a provided …","Returns the contained first value or panics with a <code>msg</code>. …","Slices utils.","Returns the contained first value or computes it from a …"],"i":[0,0,0,0,0,0],"f":[[1],[[1,2]],[1,3],[[1,4]],0,[2]],"p":[[3,"Vec"],[8,"FnOnce"],[4,"Result"],[15,"str"]]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
---
+++ doc/0.1.0/source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"rust_book14":["",[],["lib.rs"]]\
+}');
+createSourceSidebar();
